4a1b16c59378f7e6b4f554e84a6a2b427c3ea68b
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 16:18:56 -0700

    pmp

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -50,7 +50,7 @@ usertrap(void)
   // save user program counter.
   p->trapframe->epc = r_sepc();
 
-  if (r_scause() == 13 || r_scause() == 15 || r_scause() == 12) {  // Load/Store page fault
+  if (is_pmp_configured()) {  // Load/Store page fault
     uint64 faulting_addr = r_stval();
     printf("entered before pmp config\n");
     if(is_pmp_configured()) {
