8b5626a4996ad4fd8fb15229de883b9fe9abb8a7
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-05 21:55:43 -0700

    keeps repeating

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -237,8 +237,7 @@ void trap_and_emulate(void) {
 
     //ecall for prints
     if(funct3 == 0 && uimm==0x102){
-        //printf("(EC at %p)\n", p->trapframe->epc);
-        printf("entered ecall handler\n");
+        printf("(EC at %p)\n", p->trapframe->epc);
         if(vmm->current_exec_mode == VM_MODE_U)
         {
             vmm->current_exec_mode = VM_MODE_S;
