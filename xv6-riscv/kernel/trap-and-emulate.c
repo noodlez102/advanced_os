989d3e8fd797fc5f70f645e4cfce74c748ff29d2
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-05 21:14:07 -0700

    hmm

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -221,7 +221,7 @@ void trap_and_emulate(void) {
     }
 
     /* Comes here when a VM tries to execute a supervisor instruction. */
-    printf("entered trap_and_emulate\n");
+    //printf("entered trap_and_emulate\n");
     /* Retrieve all required values from the instruction */
     uint64 addr     = p->trapframe->epc;
     uint32 op       = instr & 0x7f;
@@ -252,7 +252,7 @@ void trap_and_emulate(void) {
         }
     }//SRET
     else if (funct3 == 0 && uimm == 0x102) {
-        printf("entered sret handler\n");
+        //printf("entered sret handler\n");
         uint64 value_sstatus = vmm->sstatus.val;
         uint64 spp = (value_sstatus >> 8) & 0x1;
         if(vmm->current_exec_mode != 1){
@@ -275,7 +275,7 @@ void trap_and_emulate(void) {
         }
     }//MRET
     else if (funct3 == 0 && uimm == 0x302) {
-        printf("entered mret handler\n");
+        //printf("entered mret handler\n");
         uint64 value_mstatus = vmm->mstatus.val;
         uint64 mpp = (value_mstatus >> 11) & 0x3;
         if (mpp == 3) {
@@ -292,7 +292,7 @@ void trap_and_emulate(void) {
         }
     } //csrwrite
     else if (funct3 == 0x1) {
-        printf("entered csrwrite handler\n");
+        //printf("entered csrwrite handler\n");
         struct vm_reg* found_reg = csr_register(uimm);
         if (found_reg != NULL) {
             int source_val = get_tf_reg(p->trapframe, rs1);
@@ -307,15 +307,15 @@ void trap_and_emulate(void) {
         p->trapframe->epc += 4;
     }//csrread
     else if (funct3 == 0x2) {
-        printf("entered csrread handler\n");
+        //printf("entered csrread handler\n");
         struct vm_reg* found_reg = csr_register(uimm);
         if (found_reg == NULL) {
             printf("Incorrect CSR code %x for execution mode as : %d\n", uimm, vmm->current_exec_mode);
             kill(p->pid);
         } else {
-            printf("current mode execution is: %d and the register's mode I am lloking for is: %d\n",vmm->current_exec_mode,found_reg->mode);
+            //printf("current mode execution is: %d and the register's mode I am lloking for is: %d\n",vmm->current_exec_mode,found_reg->mode);
             if(vmm->current_exec_mode >=found_reg->mode){
-                printf("right before set trapframe\n");
+                //printf("right before set trapframe\n");
                 set_tf_reg(p->trapframe, rd, found_reg->val);
             }
         }
