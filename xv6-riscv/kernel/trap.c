244688f23c840c5d3f5cd8e84302ca96f9484d32
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 21:47:30 -0700

    pmp

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -55,9 +55,13 @@ usertrap(void)
           printf("Page Fault Occured. Probably due to PMP Violation\n");
           printf("Accessing Address: %p\n", r_stval());
           kill(p->pid);
+        trap_and_emulate();
+
       } else {
           printf("usertrap(): unexpected page fault\n");
           kill(p->pid);
+          trap_and_emulate();
+
       }
   }
   if (p->proc_te_vm == 1 && (r_scause() == 2 || r_scause() == 1))
