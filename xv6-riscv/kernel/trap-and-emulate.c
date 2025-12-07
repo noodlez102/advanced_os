da15f137a18959b1c59b3ea2db5a9d5e06422e10
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 23:14:51 -0700

    pmp

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -247,52 +247,7 @@ void uvmcopy_copmp(pagetable_t old, pagetable_t new, uint64 sz){
 }
 
 void pmp_apply_rules(pagetable_t pt) {
-    uint64 prev_addr = 0;
-    
-    for(int i = 0; i < 64; i++) {
-        uint64 pmpaddr_val = vmm->pmpaddr[i].val;
-        
-        if(pmpaddr_val == 0) continue;
-        
-        // Get the config byte for this entry
-        int cfg_reg_idx = (i / 8) * 2;  // Which pmpcfg register (even only for RV64)
-        int cfg_byte_idx = i % 8;        // Which byte within that register
-        
-        uint64 pmpcfg = vmm->pmpcfg[cfg_reg_idx].val;
-        uint64 cfg_byte = (pmpcfg >> (cfg_byte_idx * 8)) & 0xFF;
-        
-        // Check A field (bits 3-4) for addressing mode
-        int A = (cfg_byte >> 3) & 0x3;
-        
-        if(A == 0) {
-            // OFF - skip
-            prev_addr = pmpaddr_val << 2;
-            continue;
-        }
-        
-        uint64 region_end = pmpaddr_val << 2;
-        uint64 region_start = prev_addr;
-        
-        // Check permissions (R=bit0, W=bit1, X=bit2)
-        // int R = cfg_byte & 0x1;
-        // int W = (cfg_byte >> 1) & 0x1;
-        // int X = (cfg_byte >> 2) & 0x1;
-        
-        // If no permissions for S/U mode, unmap this region
-        // if(R == 0 && W == 0 && X == 0) {
-            // Unmap pages in this region that fall within 0x80000000-0x80400000
-            for(uint64 va = region_start; va < region_end; va += PGSIZE) {
-                if(va >= 0x80000000 && va < 0x80400000) {
-                    pte_t *pte = walk(pt, va, 0);
-                    if(pte && (*pte & PTE_V)) {
-                        uvmunmap(pt, va, 1, 0);
-                    }
-                }
-            }
-        // }
-        
-        prev_addr = region_end;
-    }
+    uint64 base_addr = 0x80000000;
 }
 pagetable_t vmm_pagetable_backup(void){
     return vmm->backuppagetable;
@@ -326,15 +281,15 @@ void print_pmp_regions(void) {
 }
 
 void do_pmp_switch(struct proc *p){
-    vmm->backuppagetable = p->pagetable;
 
     if(vmm->pagetable == NULL) {
+        vmm->backuppagetable = p->pagetable;
         vmm->pagetable = proc_pagetable(p);
         uvmcopy_copmp(p->pagetable, vmm->pagetable, p->sz);
-        pmp_apply_rules(vmm->pagetable);
+        //pmp_apply_rules(vmm->pagetable);
+        uvmunmap(vmm->pagetable, 0x0000000080000000, 1, 0);
     }
     
-    // Save backup and switch
     p->pagetable = vmm->pagetable;
 }
 
