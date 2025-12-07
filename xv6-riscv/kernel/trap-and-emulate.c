82f84dc1437f3a0fe491300a1497181ec3e04dd0
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 23:45:07 -0700

    pmp

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -226,40 +226,32 @@ void uvmcopy_copmp(pagetable_t old, pagetable_t new, uint64 sz){
     uint flags;
     char *mem;
 
-    for(i = 0; i < sz; i += PGSIZE){
-        if((pte = walk(old, i, 0)) == 0)
-        panic("uvmcopy: pte should exist");
-        if((*pte & PTE_V) == 0)
-        panic("uvmcopy: page not present");
-        pa = PTE2PA(*pte);
-        flags = PTE_FLAGS(*pte);
-        if((mem = kalloc()) == 0)
-        goto err;
-        memmove(mem, (char*)pa, PGSIZE);
-        if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-        kfree(mem);
-        goto err;
-        }
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walk(old, i, 0)) == 0)
+      panic("uvmcopy: pte should exist");
+    if((*pte & PTE_V) == 0)
+      panic("uvmcopy: page not present");
+    pa = PTE2PA(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0){
+        printf("unable to kalloc men\n");
     }
+    memmove(mem, (char*)pa, PGSIZE);
+    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+      kfree(mem);
+    }
+  }
+
 
   for(i = 0x80000000; i < 0x80400000; i += PGSIZE){
-        if((pte = walk(old, i, 0)) == 0)
-        panic("uvmcopy: pte should exist");
-        if((*pte & PTE_V) == 0)
-        panic("uvmcopy: page not present");
-        pa = PTE2PA(*pte);
-        flags = PTE_FLAGS(*pte);
-        if((mem = kalloc()) == 0)
-        goto err;
-        memmove(mem, (char*)pa, PGSIZE);
-        if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-        kfree(mem);
-        goto err;
-        }
+    if((pte = walk(old, i, 0)) == 0)
+      panic("uvmcopy: pte should exist");
+    if((*pte & PTE_V) == 0)
+      panic("uvmcopy: page not present");
+    pa = PTE2PA(*pte);
+    flags = PTE_FLAGS(*pte);
+    mappages(new, i, PGSIZE, (uint64)pa, flags);
     }
- err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
-  return ;
 }
 
 void pmp_apply_rules(pagetable_t pt) {
