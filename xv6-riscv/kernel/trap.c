a66a68d6a763d3c51e297689320ef94226829bcc
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 20:09:15 -0700

    pmp

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -50,7 +50,7 @@ usertrap(void)
   // save user program counter.
   p->trapframe->epc = r_sepc();
   uint64 scause = r_scause();
-  printf("Scause is %d",scause);
+  printf("Scause is %d\n",scause);
   if(scause == 12 || scause == 13 || scause == 15) {  // Instruction/Load/Store page fault
       if(is_pmp_configured()) {
           printf("Page Fault Occured. Probably due to PMP Violation\n");
