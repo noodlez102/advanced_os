56f5437501fa70e00f8597eda8e3ca4d8864bfb8
Arthur Hua <arthur.hua.102@gmail.com>
2025-12-06 21:51:33 -0700

    pmp

--- xv6-riscv/vm/start.c
+++ xv6-riscv/vm/start.c
@@ -36,14 +36,14 @@ start()
 
  // CSE536: Uncomment to test PMP 
   // Test 1
-  // w_pmpaddr0(0x0);
-  // w_pmpaddr1(0x00100000 >> 2);
-  // w_pmpcfg0(0x0800);
+  w_pmpaddr0(0x0);
+  w_pmpaddr1(0x00100000 >> 2);
+  w_pmpcfg0(0x0800);
 
   // // Test 2
-    w_pmpaddr0(0x80000000 >> 2);
-    w_pmpaddr1(0x81000000 >> 2);
-    w_pmpcfg0(0x0800);
+    // w_pmpaddr0(0x80000000 >> 2);
+    // w_pmpaddr1(0x81000000 >> 2);
+    // w_pmpcfg0(0x0800);
 
   // // Test 3
   // w_pmpaddr0(0x00100000 >> 2);
